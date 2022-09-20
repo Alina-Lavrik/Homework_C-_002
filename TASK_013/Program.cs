e1fdbf903a107e9fa841f3c3f007904d118c2402
Alina Lavrik <alina_voro@mail.ru>
2022-09-20 15:44:36 +0300

    Add TASK 13

---
+++ TASK_013/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6  */
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number <= 99)
+{
+    Console.Write($"Третьей цифры нет ");
+}
+else if (number <= 999)
+{
+    int resalt = number % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+else if (number <= 9999)
+{
+    int resalt = (number / 10) % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+else if (number <= 99999)
+{
+    int resalt = (number / 100) % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+else if (number <= 999999)
+{
+    int resalt = (number / 1000) % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+else if (number <= 9999999)
+{
+    int resalt = (number / 10000) % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+else if (number <= 99999999)
+{
+    int resalt = (number / 100000) % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+else if (number <= 999999999)
+{
+    int resalt = (number / 1000000) % 10;
+    {
+        Console.Write($"Третья цифра заданного числа: {resalt}");
+    }
+}
+
+
+
+/*else 
+{
+    Console.WriteLine("Введите число поменьше");
+} */
